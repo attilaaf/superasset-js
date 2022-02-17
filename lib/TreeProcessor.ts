a3d2305514cc96cf4d82270c619d8693c2434b57
morgoth <morgoth@morgoths-MacBook-Pro.local>
2022-02-16 21:35:38 -0500

    Adding next tx logic

--- lib/TreeProcessor.ts
+++ lib/TreeProcessor.ts
@@ -7,7 +7,12 @@ import { TreeProcessorInterface } from "./interfaces/TreeProcessor.interface";
 import { RequiredTransactionPartialResult } from "./interfaces/RequiredTransactionPartialResult.interface";
 import { InvalidBnsConstantError, InvalidCharError, InvalidCurrentDimensionError, InvalidDupHashError } from "./errors/OutputErrors";
 import { buildContractClass, toHex, signTx, Ripemd160, Sig, PubKey, Bool, Bytes, compile, num2bin, getPreimage } from "scryptlib";
-import * as bnsjson from './bnsclaim_release_desc.json';
+import { bnsclaim } from "./bnsclaim_release_desc";
+import { ExtensionOutputData } from "./interfaces/ExtensionOutputData.interface";
+
+function buildNFTPublicKeyHashOut(asset, pkh) {
+    return bsv.Script.fromASM(`${asset} ${pkh} OP_NIP OP_OVER OP_HASH160 OP_EQUALVERIFY OP_CHECKSIG`);
+}
 
 /**
  * Process the transaction tree
@@ -76,6 +81,7 @@ export class TreeProcessor implements TreeProcessorInterface {
         let nameString = '';
         let prevPotentialClaimNft = '';
         let prevTx = rootTx;
+        let prevOutput: ExtensionOutputData | null = null;
         // For each letter of the name
         const extensionData = parseExtensionOutputData(rootTx, 0);
         if (extensionData?.bnsConstant !== 'bns1') {
@@ -101,7 +107,7 @@ export class TreeProcessor implements TreeProcessorInterface {
                 // Do not concat the root node, skip it
                 if (i > 1) {
                     const prevTxOut = prevTx.txOuts[outputIndex];
-                    const prevOutput = parseExtensionOutputData(prevTx, outputIndex);
+                    prevOutput = parseExtensionOutputData(prevTx, outputIndex);
                     if (!prevOutput) {
                         console.log('Should not happen prevOutput is null');
                         throw new PrefixChainMismatchError();
@@ -131,7 +137,7 @@ export class TreeProcessor implements TreeProcessorInterface {
             i = i + 1;
         }
         const nextMissingChar = name.charAt(i);
-        const requiredTx = this.buildRequiredTx(prevTx, nextMissingChar);
+        const requiredTx = this.buildRequiredTx(prevOutput, prevTx, nextMissingChar);
         // Construct the transaction as it would need to be minus the funding input and change output
         return {
             success: false,
@@ -142,10 +148,7 @@ export class TreeProcessor implements TreeProcessorInterface {
         };
     }
 
-    private buildRequiredTx(prevTx: bsv.Tx, nextMissingChar: string): bsv.Tx {
-        // -----------------------------------------------------
-        // Step 1: Deploy with initial owner and satoshis value of 2650 (Lower than this may hit dust limit)
-        // Add the output letters 
+    private buildRequiredTx(prevOutput: ExtensionOutputData, prevTx: bsv.Tx, nextMissingChar: string): bsv.Tx {
         const letters = [
             '2d',
             '5f',
@@ -189,21 +192,168 @@ export class TreeProcessor implements TreeProcessorInterface {
         ];
         const dividedSats = 800 * letters.length;
         const totalExtendOutputs = letters.length;
-    
         const FEE = 2000 + letters.length * 250;
         const INITIAL_FEE = 350;
-        console.log('FEE', FEE);
-    
         const dividedSatsResult = dividedSats / totalExtendOutputs;
         const dividedSatsResultnum2bin = num2bin(dividedSatsResult, 8);
-    
-        console.log('bnsjson', bnsjson);
         // If changing to 'release' then update the outputSize to 'f2' (to reflect smaller output size). Use 'fc' for debug.
         //const outputSize = 'fc'; // Change to fc for debug or f2 for release
-        const BNS = buildContractClass(bnsjson);
-        
+        const BNS = buildContractClass(bnsclaim());
         const tx = new bsv.Tx();
+        // const publicKeyHash = bsv.crypto.Hash.sha256ripemd160(publicKey.toBuffer())
+        /*
+        // Do NOT provide a constructor as that will add unnecessary OP_0 OP_0 to the beginning of the contract
+        bytes bnsConstant;      // Assetid is the first push data
+        Ripemd160 issuerPkh;    // Issuer can transform or destroy the nodes
+        Ripemd160 claimNftHash;// Claim NFT pkh
+        int currentDimension;   // Current dimension
+        Ripemd160 dupHash;      // Dedup hash
+        bytes char;             // current char
+        */
+        const bnsConstant = Buffer.from('bns1', 'utf8').toString('hex');
+        const issuerPkh = toHex(prevOutput.issuerPkh);
+        const claimHash = toHex(prevOutput.claimHash);
+        const claimNftScriptSCRIPT = buildNFTPublicKeyHashOut(num2bin(0, 36), prevOutput.issuerPkh);
+        const claimNftScript = claimNftScriptSCRIPT.toHex();
+        const claimSatoshisInt = 300;
+        // console.log('claimNftScript', claimNftScript, claimNftScript.length / 2);
+        const outputSize = '3f'; // SANFT: 'f2' for release' and 'fc' for debug or P2NFTPKH: 3f (63 bytes)
+        console.log('claimNftScript', claimNftScript, claimNftScript.length / 2);
+        const claimSatoshisWithFullOutput = num2bin(claimSatoshisInt, 8) + outputSize + claimNftScript;
+        const claimNftHash = bsv.crypto.Hash.ripemd160(Buffer.from(claimSatoshisWithFullOutput, 'hex')).toString('hex');  
+       // console.log('claimNftHash', claimNftHash, claimPkh.length );
+        console.log('claimSatoshisWithFullOutput', claimSatoshisWithFullOutput);
+        // const claimNftHash = toHex(nftHash);
+        let prevDupHash = '0000000000000000000000000000000000000000';
+        let currentDimension = 20;
+        const tree = new BNS(
+          new Bytes(bnsConstant),
+          new Ripemd160(issuerPkh),
+          new Ripemd160(claimNftHash),
+          new Ripemd160(prevOutput.dupHash),
+          prevOutput.currentDimension + 1,
+          new Bytes('ff')
+        );
+        const Signature = bsv.crypto.Signature;
+        const sighashTypeBns = Signature.SIGHASH_ANYONECANPAY | Signature.SIGHASH_ALL | Signature.SIGHASH_FORKID;
+        const asmVars = {
+            'Tx.checkPreimageOpt_.sigHashType': sighashTypeBns.toString(),
+        };
+        tree.replaceAsmVars(asmVars);
+        const lockingScriptSize = tree.lockingScript.toHex().length / 2;
+        console.log('lockingScriptOriginal size', lockingScriptSize, tree.lockingScript.toASM(), tree.lockingScript.toHex());
+        const dividedSatoshisBytesWithSize = new Bytes(dividedSatsResultnum2bin + 'fd' + num2bin(tree.lockingScript.toHex().length / 2, 2)); // Change to length of script
+        console.log('bnsConstant', bnsConstant);
+        console.log('issuerPkh', issuerPkh);
+        console.log('claimNftHash', claimNftHash);
+        console.log('prevDupHash', prevDupHash);
+        console.log('currentDimension', currentDimension);
+        console.log('char', 'ff');
 
+        function getLockingScriptForCharacter(letter, dimensionCount, dupHash) {
+            const slicedPrefix = tree.lockingScript.toASM().substring(0, 90);
+            const slicedSuffix = tree.lockingScript.toASM().substring(138);
+            const replaced = slicedPrefix + ' ' + dupHash + ' ' + num2bin(dimensionCount, 1) + ' ' + letter + ' ' + slicedSuffix;
+            return bsv.Script.fromASM(replaced);
+        }
+
+
+        // call contract method on testnet
+    const extendRootTx = new bsv.Transaction();
+
+    // Step 2.
+    // Spend the root 
+    console.log('About to extend root...')
+
+    await sleeper(5);
+    // Add funding input
+    //const utxo = await fetchUtxoLargeThan(privateKey.toAddress(), 300000);
+
+    extendRootTx.addInput(createInputFromPrevTx(deployTx))
+    .from(await fetchUtxos(privateKey.toAddress()))
+
+    // Create NFT claim output
+    extendRootTx.setOutput(0, (tx) => {
+      // const deployData = buildNFTPublicKeyHashOut(num2bin(0, 36), privateKey.toAddress().toHex().substring(2))
+      // Use the OP PUSH TX version of p2nftpkh
+      return new bsv.Transaction.Output({
+        script: bsv.Script.fromHex(claimNftScript),
+        satoshis: claimSatoshisInt,
+      })
+    })
+    currentDimension++;
+
+    const lockingScriptsLevel0 = {};
+    let dupHashesLevel0;
+
+    // For the initial spend we must combine the root outpoint as part of the dedup hash
+    const rootOutpoint = Buffer.from(deployTx.id, 'hex').reverse().toString('hex') + '00000000'; 
+    const combinedDupHash = prevDupHash + rootOutpoint + 'ff'; // The parent root node is 'ff' 
+    console.log('rootOutpoint', currentDimension, rootOutpoint, combinedDupHash);
+    const dupHash = bsv.crypto.Hash.ripemd160(Buffer.from(combinedDupHash, 'hex')).toString('hex');
+    console.log('dupHash', dupHash);
+    for (let i = 0; i < letters.length; i++) {
+      let letter = letters[i];
+      dupHashesLevel0 = dupHash;
+      const newLockingScript = getLockingScriptForCharacter(letter, currentDimension, dupHash);
+      lockingScriptsLevel0[letter] = newLockingScript;
+      const lockingScriptSizeNew = newLockingScript.toHex().length / 2;
+
+      if (i == 0) {
+        console.log('tree before', tree.lockingScript.toASM());
+        console.log('lockingScriptSizeNew size', lockingScriptSizeNew, newLockingScript.toASM(), newLockingScript.toHex(), num2bin(lockingScriptSizeNew, 2));
+      }
+
+      step2ExtendLockingScripts.push({
+        newLockingScript,
+        dupHash
+      });
+
+      extendRootTx.setOutput(i + 1, (tx) => {
+        return new bsv.Transaction.Output({
+          script: newLockingScript,
+          satoshis: dividedSatsResult,
+        })
+      })
+    }
+    extendRootTx.change(privateKey.toAddress());
+
+    extendRootTx.setInputScript(0, (tx, output) => {
+      const preimage = generatePreimage(true, tx, prevLockingScript, output.satoshis, sighashTypeBns);
+      const changeAddress = new Bytes(privateKey.toAddress().toHex().substring(2));
+      const changeSatoshis = num2bin(tx.getChangeAmount(), 8);
+      const issuerPubKey = new Bytes('0000');
+      // Signature is only needed for release
+      // const sig = signTx(tx, privateKey, output.script, output.satoshis, 0, sighashTypeBns);
+      const issuerSig = new Bytes('0000');
+
+      console.log('bnsConstant', bnsConstant);
+      console.log('issuerPkh', issuerPkh);
+      console.log('claimNftHash', claimNftHash);
+      console.log('currentDimension', currentDimension);
+      console.log('prevDupHash', prevDupHash);
+      console.log('char', 'ff');
+      console.log('preimage', preimage);
+      console.log('dividedSatoshisBytesWithSize', dividedSatoshisBytesWithSize);
+      console.log('claimSatoshisWithFullOutput', claimSatoshisWithFullOutput);
+      console.log('changeAddress', changeAddress);
+      console.log('changeSatoshis', changeSatoshis);
+      console.log('isTransform', new Bool(false));
+      console.log('issuerSig', issuerSig);
+      console.log('issuerPubKey', issuerPubKey);
+      // Attach the claim NFT manually to extend
+      return tree.extend(
+        preimage,
+        dividedSatoshisBytesWithSize,
+        new Bytes(claimSatoshisWithFullOutput),
+        changeAddress,
+        new Bytes(changeSatoshis),
+        new Bool(false),
+        issuerSig,
+        issuerPubKey).toScript()
+    })
+
+    console.log('extendRootTx', extendRootTx);
         return null;
     }
 }
---
+++ lib/bnsclaim_release_desc.ts
@@ -0,0 +1,107 @@
+export function bnsclaim() {
+    return {
+        "version": 7,
+        "compilerVersion": "1.7.0+commit.c3c53bb",
+        "contract": "BNS",
+        "md5": "62038da5e0d77a7a4cbf7e58e27599e5",
+        "structs": [],
+        "alias": [
+            {
+                "name": "PubKeyHash",
+                "type": "Ripemd160"
+            }
+        ],
+        "generics": [
+            {
+                "genericTypes": [
+                    "__SCRYPT_K__",
+                    "__SCRYPT_V__"
+                ],
+                "library": "HashedMap"
+            },
+            {
+                "genericTypes": [
+                    "__SCRYPT_E__"
+                ],
+                "library": "HashedSet"
+            }
+        ],
+        "abi": [
+            {
+                "type": "function",
+                "name": "extend",
+                "index": 0,
+                "params": [
+                    {
+                        "name": "txPreimage",
+                        "type": "SigHashPreimage"
+                    },
+                    {
+                        "name": "dividedSatoshisBytesWithSize",
+                        "type": "bytes"
+                    },
+                    {
+                        "name": "claimNFT",
+                        "type": "bytes"
+                    },
+                    {
+                        "name": "changeAddress",
+                        "type": "bytes"
+                    },
+                    {
+                        "name": "changeSatoshis",
+                        "type": "bytes"
+                    },
+                    {
+                        "name": "isTransform",
+                        "type": "bool"
+                    },
+                    {
+                        "name": "issuerSig",
+                        "type": "bytes"
+                    },
+                    {
+                        "name": "issuerPubKey",
+                        "type": "bytes"
+                    }
+                ]
+            },
+            {
+                "type": "constructor",
+                "params": [
+                    {
+                        "name": "bnsConstant",
+                        "type": "bytes"
+                    },
+                    {
+                        "name": "issuerPkh",
+                        "type": "Ripemd160"
+                    },
+                    {
+                        "name": "claimHash",
+                        "type": "Ripemd160"
+                    },
+                    {
+                        "name": "dupHash",
+                        "type": "Ripemd160"
+                    },
+                    {
+                        "name": "currentDimension",
+                        "type": "int"
+                    },
+                    {
+                        "name": "char",
+                        "type": "bytes"
+                    }
+                ]
+            }
+        ],
+        "stateProps": [],
+        "buildType": "release",
+        "file": "",
+        "asm": "$bnsConstant $issuerPkh $claimHash $dupHash $currentDimension $char OP_8 OP_PICK OP_NOTIF OP_OVER 54 OP_LESSTHANOREQUAL OP_VERIFY OP_3DUP OP_DROP 14 OP_NUMEQUAL OP_IF OP_DUP OP_15 OP_PICK 68 OP_SPLIT OP_DROP 44 OP_SPLIT OP_NIP OP_CAT OP_NIP OP_ENDIF OP_13 OP_PICK OP_4 OP_CAT OP_7 OP_PICK OP_CAT 14 OP_CAT OP_6 OP_PICK OP_CAT 14 OP_CAT OP_5 OP_PICK OP_CAT 14 OP_CAT OP_OVER OP_16 OP_PICK b300 OP_SPLIT OP_DROP b200 OP_SPLIT OP_NIP OP_CAT OP_RIPEMD160 OP_CAT OP_1 OP_CAT OP_3 OP_PICK OP_1ADD OP_1 OP_NUM2BIN OP_CAT OP_1 OP_CAT OP_15 OP_PICK 6b 11 OP_PICK 6b OP_SPLIT OP_DROP 69 OP_SPLIT OP_NIP 00 OP_CAT OP_BIN2NUM OP_ADD OP_SPLIT OP_DROP b300 OP_SPLIT OP_NIP OP_6 OP_PICK OP_15 OP_PICK OP_RIPEMD160 OP_EQUALVERIFY OP_14 OP_PICK OP_2 OP_PICK OP_CAT 2d OP_CAT OP_OVER OP_CAT OP_2 OP_PICK OP_CAT 5f OP_CAT OP_OVER OP_CAT OP_2 OP_PICK OP_CAT 30 OP_CAT OP_OVER OP_CAT OP_2 OP_PICK OP_CAT 31 OP_CAT OP_OVER OP_CAT OP_2 OP_PICK OP_CAT 32 OP_CAT OP_OVER OP_CAT OP_2 OP_PICK OP_CAT 33 OP_CAT OP_OVER OP_CAT OP_2 OP_PICK OP_CAT 34 OP_CAT OP_OVER OP_CAT OP_2 OP_PICK OP_CAT 35 OP_CAT OP_OVER OP_CAT OP_2 OP_PICK OP_CAT 36 OP_CAT OP_OVER OP_CAT OP_2 OP_PICK OP_CAT 37 OP_CAT OP_OVER OP_CAT OP_2 OP_PICK OP_CAT 38 OP_CAT OP_OVER OP_CAT OP_2 OP_PICK OP_CAT 39 OP_CAT OP_OVER OP_CAT OP_2 OP_PICK OP_CAT 61 OP_CAT OP_OVER OP_CAT OP_2 OP_PICK OP_CAT 62 OP_CAT OP_OVER OP_CAT OP_2 OP_PICK OP_CAT 63 OP_CAT OP_OVER OP_CAT OP_2 OP_PICK OP_CAT 64 OP_CAT OP_OVER OP_CAT OP_2 OP_PICK OP_CAT 65 OP_CAT OP_OVER OP_CAT OP_2 OP_PICK OP_CAT 66 OP_CAT OP_OVER OP_CAT OP_2 OP_PICK OP_CAT 67 OP_CAT OP_OVER OP_CAT OP_2 OP_PICK OP_CAT 68 OP_CAT OP_OVER OP_CAT OP_2 OP_PICK OP_CAT 69 OP_CAT OP_OVER OP_CAT OP_2 OP_PICK OP_CAT 6a OP_CAT OP_OVER OP_CAT OP_2 OP_PICK OP_CAT 6b OP_CAT OP_OVER OP_CAT OP_2 OP_PICK OP_CAT 6c OP_CAT OP_OVER OP_CAT OP_2 OP_PICK OP_CAT 6d OP_CAT OP_OVER OP_CAT OP_2 OP_PICK OP_CAT 6e OP_CAT OP_OVER OP_CAT OP_2 OP_PICK OP_CAT 6f OP_CAT OP_OVER OP_CAT OP_2 OP_PICK OP_CAT 70 OP_CAT OP_OVER OP_CAT OP_2 OP_PICK OP_CAT 71 OP_CAT OP_OVER OP_CAT OP_2 OP_PICK OP_CAT 72 OP_CAT OP_OVER OP_CAT OP_2 OP_PICK OP_CAT 73 OP_CAT OP_OVER OP_CAT OP_2 OP_PICK OP_CAT 74 OP_CAT OP_OVER OP_CAT OP_2 OP_PICK OP_CAT 75 OP_CAT OP_OVER OP_CAT OP_2 OP_PICK OP_CAT 76 OP_CAT OP_OVER OP_CAT OP_2 OP_PICK OP_CAT 77 OP_CAT OP_OVER OP_CAT OP_2 OP_PICK OP_CAT 78 OP_CAT OP_OVER OP_CAT OP_2 OP_PICK OP_CAT 79 OP_CAT OP_OVER OP_CAT OP_2 OP_PICK OP_CAT 7a OP_CAT OP_OVER OP_CAT OP_13 OP_PICK OP_CAT 1976a914 OP_CAT OP_14 OP_PICK OP_CAT 88ac OP_CAT OP_HASH256 11 OP_PICK 12 OP_PICK OP_SIZE OP_NIP OP_8 OP_SUB OP_SPLIT OP_DROP 12 OP_PICK OP_SIZE OP_NIP 28 OP_SUB OP_SPLIT OP_NIP OP_EQUALVERIFY OP_2DROP OP_DROP OP_ELSE OP_6 OP_PICK OP_HASH160 OP_5 OP_PICK OP_EQUALVERIFY OP_7 OP_PICK OP_7 OP_PICK OP_CHECKSIGVERIFY OP_ENDIF OP_13 OP_PICK OP_HASH256 OP_1 OP_SPLIT OP_SWAP OP_BIN2NUM OP_1ADD OP_SWAP OP_CAT 3044022079be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f817980220 OP_SWAP OP_CAT $Tx.checkPreimageOpt_.sigHashType OP_CAT 02b405d7f0322a89d0f9f3a98e6f938fdc1c969a8d1382a2bf66a71ae74a1e83b0 OP_CHECKSIG OP_NIP OP_NIP OP_NIP OP_NIP OP_NIP OP_NIP OP_NIP OP_NIP OP_NIP OP_NIP OP_NIP OP_NIP OP_NIP OP_NIP",
+        "hex": "<bnsConstant><issuerPkh><claimHash><dupHash><currentDimension><char>587964780154a1696f7501149c63765f7901687f7501447f777e77685d79547e57797e01147e56797e01147e55797e01147e78607902b3007f7502b2007f777ea67e517e53798b51807e517e5f79016b011179016b7f7501697f7701007e81937f7502b3007f7756795f79a6885e7952797e012d7e787e52797e015f7e787e52797e01307e787e52797e01317e787e52797e01327e787e52797e01337e787e52797e01347e787e52797e01357e787e52797e01367e787e52797e01377e787e52797e01387e787e52797e01397e787e52797e01617e787e52797e01627e787e52797e01637e787e52797e01647e787e52797e01657e787e52797e01667e787e52797e01677e787e52797e01687e787e52797e01697e787e52797e016a7e787e52797e016b7e787e52797e016c7e787e52797e016d7e787e52797e016e7e787e52797e016f7e787e52797e01707e787e52797e01717e787e52797e01727e787e52797e01737e787e52797e01747e787e52797e01757e787e52797e01767e787e52797e01777e787e52797e01787e787e52797e01797e787e52797e017a7e787e5d797e041976a9147e5e797e0288ac7eaa011179011279827758947f7501127982770128947f77886d75675679a955798857795779ad685d79aa517f7c818b7c7e263044022079be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f8179802207c7e<Tx.checkPreimageOpt_.sigHashType>7e2102b405d7f0322a89d0f9f3a98e6f938fdc1c969a8d1382a2bf66a71ae74a1e83b0ac7777777777777777777777777777",
+        "sources": [],
+        "sourceMap": []
+    }
+};
