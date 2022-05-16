163ac26f78cf26c78aedbc2089fb1d26b830ce86
morgoth <morgoth@morgoths-MacBook-Pro.local>
2022-05-16 10:44:24 -0500

    Launch miner

---
+++ minerPrivateKey.js
@@ -0,0 +1,96 @@
+
+const { bsv } = require('scryptlib');
+const bip39 = require('bip39');
+const fs = require('fs');
+const crypto = require('crypto');
+
+var randomBytes = crypto.randomBytes(16); // 128 bits is enough
+var mnemonic = bip39.entropyToMnemonic(randomBytes.toString('hex'));
+
+bip39.validateMnemonic(mnemonic);
+console.log('mnemonic random: ', mnemonic);
+const jsonFileReader = async (filePath) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, (err, fileData) => {
+      if (err) {
+        return reject(err);
+      }
+      try {
+        const object = JSON.parse(fileData)
+        return resolve(object);
+      } catch (err) {
+        console.log('jsonFileReader', err);
+        return reject(null);
+      }
+    })
+  });
+}
+
+const jsonFileWriter = async (filePath, data) => {
+  return new Promise(function (resolve, reject) {
+    fs.writeFile(filePath, JSON.stringify(data), 'utf8', function (err) {
+      if (err) {
+        console.log('jsonFileWriter', err);
+        reject(err);
+      }
+      else resolve();
+    });
+  })
+};
+
+const getCurrentAddressIndex = async () => {
+  const result = await jsonFileReader('./minerIndex.txt');
+  return result.index;
+};
+
+const updateCurrentAddressIndex = async (val) => {
+  jsonFileWriter('./minerIndex.txt', {
+    index: val
+  });
+};
+
+const generateKeysFromPhrase = (phrase, index) => {
+  const seed = bip39.mnemonicToSeedSync(phrase);
+  const hdPrivateKey = bsv.HDPrivateKey.fromSeed(seed, bsv.Networks.mainnet);
+  const hdPublicKey = bsv.HDPublicKey.fromHDPrivateKey(hdPrivateKey)
+  const derived = [];
+  const childn = hdPrivateKey.deriveChild(`m/44'/0'/0'/0/` + index);
+  derived.push({
+    'path': '0/' + index,
+    'address': childn.publicKey.toAddress().toString(),
+    'publicKey': childn.publicKey,
+    'privateKey': childn.privateKey,
+    'privateKeyWif': childn.privateKey.toWIF(),
+  });
+
+ 
+  return {
+    xpub: hdPublicKey.toString(),
+    xprv: hdPrivateKey.toString(),
+    address: derived[0].address,
+    addressPath: derived[0].path,
+    addressPublicKey: derived[0].publicKey,
+    addressPrivateKey: derived[0].privateKey,
+    derivedChildren: derived
+  }
+}
+
+// generateKeysFromPhrase();
+const getNextAddressFromIndex = async () => {
+  if (!process.env.phrase) {
+    throw new Error('no phrase set');
+  }
+  const currentIndex = await getCurrentAddressIndex();
+  const keys = generateKeysFromPhrase(process.env.phrase, currentIndex);
+  return keys.address;
+};
+
+const incrementAddressIndex = async () => {
+  const currentIndex = await getCurrentAddressIndex();
+  await updateCurrentAddressIndex(currentIndex + 1);
+};
+
+module.exports = {
+  getNextAddressFromIndex,
+  incrementAddressIndex
+}
---
+++ start_minerd.js
@@ -0,0 +1,45 @@
+'use strict';
+const spawn = require('child_process').spawn;
+const minerPrivateKey = require('./minerPrivateKey.js');
+
+
+const options = {
+  stdio: [
+    'inherit', // StdIn.
+    'pipe',    // StdOut.
+    'pipe'     // StdErr.
+  ],
+};
+
+let proc = null;
+let isRunning = false;
+const spawnMiner = async (addressStr)  => {
+  console.log('Spawning...', addressStr)
+  // ./minerd --threads=9 --coinbase-addr=18FuiFFQTVKU5W32EMzVHdBHcCFhZLUg1d -o 127.0.0.1:8332 -O USER:PASSWORD --no-longpoll --no-getwork  --no-stratum --algo=sha256d --debug
+  proc = spawn('./minerd', [`--coinbase-addr=${addressStr}`, '-o', '127.0.0.1:8332', '-O', 'USER:PASSWORD', '--no-longpoll', '--no-getwork', '--no-stratum', '--algo=sha256d', '--debug'], options);
+  isRunning = true;
+  let output = '';
+  proc.stderr.on('data', (chunk) => {
+    console.log('Line: ', chunk.toString())
+    if (/accept/i.test(chunk.toString())) {
+      console.log('Found block: ', chunk.toString());
+      minerPrivateKey.incrementAddressIndex();
+      proc.kill('SIGINT');
+    }
+  });
+  proc.on('exit', () => {
+    console.log('Exit...');
+    isRunning = false;
+  });
+}
+
+setInterval(async () => {
+  console.log('Checking if still alive', (new Date().toString()), '...');
+  if (!isRunning) {
+    spawnMiner(await minerPrivateKey.getNextAddressFromIndex());
+  } else {
+    console.log('...alive');
+  }
+}, 5000);
+
+console.log('---', (new Date));
